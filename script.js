ec371ed66e1ef725ba225af7db392aa6b7f383cd
Dipti2021 <77368913+Dipti2021@users.noreply.github.com>
2021-03-04 16:28:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment Code
-
 //Variable declarations
 var pwd_length; 
 var pwd='';
@@ -13,10 +12,11 @@ var sym;
 var lower_alphabets =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper_alphabets= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
+//Function definition and function declaration
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -27,7 +27,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//Function declaration
+//Function calling
 function generatePassword() {
   //prompting the number of character that can be used to generate the password
   pwd_length = prompt("Choose a number between 8 and 128 to determine the length of your password");
@@ -50,53 +50,54 @@ function generatePassword() {
   
 
 //combinations for generating the password using all the characters 
+  // if none of the condition is true then the process will stop 
   if (!lower && !upper && !num && !sym) {
     pwd = alert("See you soon 👍");
   }
-  else if (lower && upper && num && sym) {
+  else if (lower && upper && num && sym) {// if all the four conditions are true, then a password will be generated which will have all the four special  characters
     pwd = lower_alphabets.concat(upper_alphabets, numbers, symbols);
     
     
-  } else if (lower && upper && num) {
+  } else if (lower && upper && num) {// if lower, upper and num conditions are true, then a password will be generated which will have these three special characters only
     pwd  = lower_alphabets.concat(upper_alphabets, numbers);
     
-  } else if (lower && upper && sym) {
+  } else if (lower && upper && sym) {// if lower, upper and sym conditions are true, then a password will be generated which will have these three special characters only
     pwd  = lower_alphabets.concat(upper_alphabets, symbols);
     
-  } else if (lower && num && sym) {
+  } else if (lower && num && sym) {// if lower, sym and num conditions are true, then a password will be generated which will have these three special characters only
     pwd  = lower_alphabets.concat(numbers, symbols);
     
-  } else if (upper && num && sym) {
+  } else if (upper && num && sym) {// if upper and num and sym conditions are true, then a password will be generated which will have these three special characters only
     pwd  = upper_alphabets.concat(numbers, symbols);
    
-  } else if (lower && upper) {
+  } else if (lower && upper) {// if lower and upper conditions are true, then a password will be generated which will have these two special characters only
     pwd  = lower_alphabets.concat(upper_alphabets);
     
-  } else if (lower && num) {
+  } else if (lower && num) {// if lower and num conditions are true, then a password will be generated which will have these two special characters only
     pwd  = lower_alphabets.concat(numbers);
     
-  } else if (lower && sym) {
+  } else if (lower && sym) { // if lower and sym conditions are true, then a password will be generated which will have these two special characters only
     pwd  = lower_alphabets.concat(symbols);
    
-  } else if (upper && num) {
+  } else if (upper && num) { // if upper and num conditions are true, then a password will be generated which will have these two special characters only
     pwd  = upper_alphabets.concat(numbers);
     
-  } else if (upper && sym) {
+  } else if (upper && sym) {// if upper and sym conditions are true, then a password will be generated which will have these two special characters only
     pwd  = upper_alphabets.concat(symbols);
    
-  } else if (num && sym) {
+  } else if (num && sym) {// if num and num conditions are true, then a password will be generated which will have these two special characters only
    pwd  = numbers.concat(symbols);
    
-  } else if (lower) {
+  } else if (lower) {// if lower condition is true, then a password will be generated which will have only this special character
     pwd  = lower_alphabets;
    
-  } else if (upper) {
+  } else if (upper) {// if upper condition is true, then a password will be generated which will have only this special character
     pwd  = upper_alphabets;
     
-  } else if (num) {
+  } else if (num) {// if num condition is true, then a password will be generated which will have only this special character
     pwd  = numbers;
     
-  } else if (sym) {
+  } else if (sym) {// if sym condition is true, then a password will be generated which will have only this special character
     pwd  = symbols;
     
   }
@@ -106,8 +107,9 @@ function generatePassword() {
   
 
   //combining and genereating a random password which will be stored and returned as a new variable
+  //for loop will give us a value for the password length
   for (var i = 0; i < pwd_length; i++) {
-    empty_pwd += pwd[Math.floor(Math.random() * pwd.length)];
+    empty_pwd += pwd[Math.floor(Math.random() * pwd.length)];//math.random() function will generate a random float number which when multiplied with the password length will generate a new random length and math.floor() function will change that float number to its closest and greatest number variant.and a random password of that new enth will be generated.
     console.log(empty_pwd);
   }
   return empty_pwd;
